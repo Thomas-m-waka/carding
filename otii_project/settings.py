4dd755b3226e3bf680de6879acb31e1d6a949d8c
Thomas Muteti <79853464+Thomas-m-waka@users.noreply.github.com>
2024-05-23 16:25:10 +0300

    Update settings.py

--- otii_project/settings.py
+++ otii_project/settings.py
@@ -45,10 +45,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -137,6 +137,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = ( os.path.join(BASE_DIR,'static/'),)
+STATIC_ROOT =  os.path.join(BASE_DIR,'static/')
 
 
